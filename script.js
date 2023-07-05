c763e7851e282c81487ae1227ad3a489c4dd7ce7
Guri <gurvinder.12011048@lpu.in>
2023-07-06 00:45:12 +0530

    mobile

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // let menu = document.querySelector('#menu-icon');
-// let navlist = document.querySelector('.navlist');
+// let navlist = document.querySelector('.nav-belt-right');
 
 // menu.onclick =() =>{
 //     menu.classList.toggle('bx-x');
@@ -13,7 +13,7 @@ const sr = ScrollReveal({
     reset : true
 });
 
-sr.reveal('.card-title',{delay : 200 , origin : 'top'});
-sr.reveal('.card-gxuri',{delay : 450 , origin : 'top'});
-sr.reveal('.card-cc',{delay : 950 , origin : 'left'});
-sr.reveal('.nav-belt',{delay : 950 , origin : 'right'});
+sr.reveal('.card-title',{delay : 200 , origin : 'left'});
+sr.reveal('.card-gxuri',{delay : 450 , origin : 'right'});
+sr.reveal('.card-cc',{delay : 950 , origin : 'bottom'});
+sr.reveal('.nav-belt',{delay : 950 , origin : 'top'});
